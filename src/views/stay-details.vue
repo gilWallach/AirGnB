4f627c77e86c8a6c0a02d42c8f1f490761cde9c6
Raz Israeli <razisraeli1@gmail.com>
2022-11-30 17:26:09 +0200

    details cmp ui improvements

--- src/views/stay-details.vue
+++ src/views/stay-details.vue
@@ -1,18 +1,37 @@
 <template>
+  <!-- HEADER -->
   <section v-if="stay" class="stay-details">
     <div class="details-header">
       <h1>{{ getStay.name }}</h1>
     </div>
-    <div class="details-ratings-container">
-      <p class="rate"><star /> 4.82 ·&nbsp;</p>
-      <p class="reviews-amount">
-        {{ getStay.reviews?.length }} reviews ·&nbsp;
-      </p>
-      <p class="address">{{ getStay.loc.city }}, {{ getStay.loc.country }}</p>
+
+    <!-- STAY DETAILS AND RATINGS -->
+    <div class="stay-details-container">
+      <div class="details-ratings-container">
+        <p class="rate">
+          <star /><span> 4.82 </span><span class="separator">·</span>
+        </p>
+        <p>
+          <span class="reviews-amount"
+            >{{ getStay.reviews?.length }} reviews
+          </span>
+          <span class="separator">·</span>
+        </p>
+        <p class="address">{{ getStay.loc.city }}, {{ getStay.loc.country }}</p>
+      </div>
+
+      <!-- SHARE AND SAVE -->
+      <div class="action-btns">
+        <div class="share action-btn"><share /> <span>Share</span></div>
+        <div class="save action-btn"><save /> <span>Save</span></div>
+      </div>
     </div>
+
+    <!-- IMAGE GALLERY -->
     <div class="gallery">
       <img v-for="(img, idx) in getStay.imgUrls" :src="img" alt="stay-image" />
     </div>
+
     <pre>{{ getStay }}</pre>
   </section>
   <section v-else class="loading">Loading....</section>
@@ -20,6 +39,8 @@
 
 <script>
 import star from '../assets/svg/star.vue'
+import share from '../assets/svg/share.vue'
+import save from '../assets/svg/save.vue'
 export default {
   name: 'stay-details',
   props: {},
@@ -42,6 +63,8 @@ export default {
   },
   components: {
     star,
+    share,
+    save,
   },
 }
 </script>
