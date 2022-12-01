9c061940150a7ea5f8e65924a82b9456891f41cc
Bar Ben Shimol <barbenshimol2@gmail.com>
2022-12-01 11:44:50 +0200

    layout changes

--- src/cmps/app-header.vue
+++ src/cmps/app-header.vue
@@ -1,44 +1,47 @@
 <template>
-  <header class="main-header flex align-center justify-between">
-    <div class="logo">
-      <router-link to="/">
-        <span role="img" aria-label="logo" class="flex align-center">
-          <airbnb />
-          AirGnB
-        </span>
-      </router-link>
-    </div>
-    <div class="mini-search flex align-center" @click="isSearchOpen = !isSearchOpen">
-      <button>Anywhere</button>
-      <div class="break-line"></div>
-      <button>Any Week</button>
-      <div class="break-line"></div>
-      <button>Add guests</button>
-      <div class="search flex align-center justify-center">
-        <search />
+  <div :class="{ 'main-layout-list': isList, 'main-container-stay-details': !isList }">
+
+    <header class="main-header flex align-center justify-between ">
+      <div class="logo">
+        <router-link to="/">
+          <span role="img" aria-label="logo" class="flex align-center">
+            <airbnb />
+            AirGnB
+          </span>
+        </router-link>
       </div>
-    </div>
-    <!-- <router-link to="/">Stays</router-link>
+      <div class="mini-search flex align-center" @click="isSearchOpen = !isSearchOpen">
+        <button>Anywhere</button>
+        <div class="break-line"></div>
+        <button>Any Week</button>
+        <div class="break-line"></div>
+        <button>Add guests</button>
+        <div class="search flex align-center justify-center">
+          <search />
+        </div>
+      </div>
+      <!-- <router-link to="/">Stays</router-link>
       <router-link to="/review">Reviews</router-link>
       <router-link to="/chat">Chat</router-link>
       <router-link to="/login">Login / Signup</router-link> -->
-    <nav class="main-nav flex align-center">
-      <hamburger />
-      <div class="profile-img flex align-center">
-        <img src="https://a0.muscache.com/defaults/user_pic-50x50.png?v=3" alt="" />
-      </div>
-    </nav>
-    <section class="loggedin-user" v-if="loggedInUser">
-      <router-link :to="`/user/${loggedInUser._id}`">
-        {{ loggedInUser.fullname }}
-      </router-link>
-      <span>{{ loggedInUser.score.toLocaleString() }}</span>
-      <img :src="loggedInUser.imgUrl" />
-    </section>
-  </header>
-  <transition name="fade">
-    <secondary-header v-if="isSearchOpen"/>
-  </transition>
+      <nav class="main-nav flex align-center">
+        <hamburger />
+        <div class="profile-img flex align-center">
+          <img src="https://a0.muscache.com/defaults/user_pic-50x50.png?v=3" alt="" />
+        </div>
+      </nav>
+      <section class="loggedin-user" v-if="loggedInUser">
+        <router-link :to="`/user/${loggedInUser._id}`">
+          {{ loggedInUser.fullname }}
+        </router-link>
+        <span>{{ loggedInUser.score.toLocaleString() }}</span>
+        <img :src="loggedInUser.imgUrl" />
+      </section>
+    </header>
+    <transition name="fade">
+      <secondary-header v-if="isSearchOpen" />
+    </transition>
+  </div>
 </template>
 <script>
 import airbnb from '../assets/svg/airbnb.vue'
@@ -49,13 +52,19 @@ import secondaryHeader from './secondary-header.vue'
 export default {
   data() {
     return {
-      isSearchOpen: false
+      isSearchOpen: false,
     }
+  },
+  created(){
+
   },
   computed: {
     loggedInUser() {
       return this.$store.getters.loggedinUser
     },
+    isList(){
+      return this.$store.getters.isList
+    }
   },
   components: {
     airbnb,
---
+++ src/cmps/date-picker.vue
@@ -0,0 +1,97 @@
+<template>
+    <div class="demo-date-picker">
+        <div class="block">
+            <!-- <span class="demonstration">Default</span> -->
+            <el-date-picker v-model="dates" type="daterange" unlink-panels range-separator="|"
+                start-placeholder="Add dates" end-placeholder="Add dates" clear-icon :prefix-icon="customPrefix"
+                :disabled-date="disableDates($event)" @change="setDates" @calendar-change="showDate" format="MMM D" value-format="x"/>
+        </div>
+        <!-- If I want to add +- days next add :shortcuts property -->
+    </div>
+</template>
+  
+<script>
+
+export default {
+    data() {
+        return {
+            dates: [,],
+            value1: [],
+            shortcuts: [
+                {
+                    text: 'Last week',
+                    value: () => {
+                        end = new Date()
+                        start = new Date()
+                        start.setTime(start.getTime() - 3600 * 1000 * 24 * 7)
+                        return [start, end]
+                    },
+                },
+                {
+                    text: 'Last month',
+                    value: () => {
+                        end = new Date()
+                        start = new Date()
+                        start.setTime(start.getTime() - 3600 * 1000 * 24 * 30)
+                        return [start, end]
+                    },
+                },
+                {
+                    text: 'Last 3 months',
+                    value: () => {
+                        end = new Date()
+                        start = new Date()
+                        start.setTime(start.getTime() - 3600 * 1000 * 24 * 90)
+                        return [start, end]
+                    },
+                },
+            ]
+        }
+    },
+    methods: {
+        showDate(dates) {
+            console.log("🚀 ~ file: date-picker.vue:54 ~ showDate ~ dates", dates[0].toString().substring(4,10),dates[1])
+
+            // console.log('date:',this.value1[0])
+        },
+        setDates(){
+            // console.log(this.dates)
+        },
+        disableDates(){
+            console.log(arguments);
+        }
+    },
+    computed: {
+        customPrefix() {
+            return 'h'
+        }
+    }
+}
+</script>
+<style scoped>
+.demo-date-picker {
+    display: flex;
+    width: 100%;
+    padding: 0;
+    flex-wrap: wrap;
+}
+
+.demo-date-picker .block {
+    padding: 30px 0;
+    text-align: center;
+    border-right: solid 1px var(--el-border-color);
+    flex: 1;
+}
+
+.demo-date-picker .block:last-child {
+    border-right: none;
+}
+
+.demo-date-picker .demonstration {
+    display: block;
+    color: var(--el-text-color-secondary);
+    font-size: 14px;
+    margin-bottom: 20px;
+}
+</style>
+  
