ddcfa3d48c0301f40e133273fe95a76071255fce
Gil Wallach <gilwallach@gmail.com>
2022-12-01 14:16:20 +0200

    cards - wishlist & carousel improved

--- src/cmps/stay-carousel.vue
+++ src/cmps/stay-carousel.vue
@@ -1,6 +1,6 @@
 <template>
 <section class='stay-carousel'>
-    <el-carousel trigger.stop="click" :autoplay="false">
+    <el-carousel trigger="click" :autoplay="false">
       <el-carousel-item v-for="img in stay.imgUrls" :key="img" >
         <img :src="img" alt="stay imgs" class="card-image">
       </el-carousel-item>
@@ -18,8 +18,7 @@ name: 'stay-carousel',
   created(){},
  methods: {},
  computed: {},
- components: {
-    
+ components: { 
  }
   }
  </script>
