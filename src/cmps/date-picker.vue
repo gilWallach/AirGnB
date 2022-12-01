68aaa4ff4833b4e0a572bf6547503e2901e41b0f
Bar Ben Shimol <barbenshimol2@gmail.com>
2022-12-01 14:18:44 +0200

    date-picker

--- src/cmps/date-picker.vue
+++ src/cmps/date-picker.vue
@@ -2,9 +2,10 @@
     <div class="demo-date-picker">
         <div class="block">
             <!-- <span class="demonstration">Default</span> -->
-            <el-date-picker v-model="dates" type="daterange" unlink-panels range-separator="|"
+            <el-date-picker v-model="dates" type="daterange" range-separator="|"
                 start-placeholder="Add dates" end-placeholder="Add dates" clear-icon :prefix-icon="customPrefix"
-                :disabled-date="disableDates($event)" @change="setDates" @calendar-change="showDate" format="MMM D" value-format="x"/>
+                :disabled-date="disabledDate" @change="setDates" @calendar-change="showDate" format="MMM D"
+                value-format="x" />
         </div>
         <!-- If I want to add +- days next add :shortcuts property -->
     </div>
@@ -16,7 +17,6 @@ export default {
     data() {
         return {
             dates: [,],
-            value1: [],
             shortcuts: [
                 {
                     text: 'Last week',
@@ -50,48 +50,30 @@ export default {
     },
     methods: {
         showDate(dates) {
-            console.log("🚀 ~ file: date-picker.vue:54 ~ showDate ~ dates", dates[0].toString().substring(4,10),dates[1])
+            const inTS = new Date(dates[0])?.getTime() || null
+            const outTS = new Date(dates[1])?.getTime() || null
+            // if(inTS) this.dates.push(inTS)
+            if (inTS) this.dates[0] = inTS
+            if (outTS) this.dates[1] = outTS
+            console.log(outTS);
+            // this.dates = [inTS,outTS]
+            // this.dates = dates
 
             // console.log('date:',this.value1[0])
         },
-        setDates(){
-            // console.log(this.dates)
+        setDates() {
+            console.log(this.dates)
         },
-        disableDates(){
-            console.log(arguments);
+        disabledDate(time) {
+            const date = new Date();
+            const previousDate = date.setDate(date.getDate() - 1);
+            return time.getTime() <= previousDate
         }
     },
     computed: {
         customPrefix() {
             return 'h'
-        }
+        },
     }
 }
 </script>
-<style scoped>
-.demo-date-picker {
-    display: flex;
-    width: 100%;
-    padding: 0;
-    flex-wrap: wrap;
-}
-
-.demo-date-picker .block {
-    padding: 30px 0;
-    text-align: center;
-    border-right: solid 1px var(--el-border-color);
-    flex: 1;
-}
-
-.demo-date-picker .block:last-child {
-    border-right: none;
-}
-
-.demo-date-picker .demonstration {
-    display: block;
-    color: var(--el-text-color-secondary);
-    font-size: 14px;
-    margin-bottom: 20px;
-}
-</style>
-  
