ea7e811af9037d3d3c315eb3f3366046d04b8529
Bar Ben Shimol <barbenshimol2@gmail.com>
2022-12-02 18:45:40 +0200

    header and date-picker css

--- src/cmps/secondary-header.vue
+++ src/cmps/secondary-header.vue
@@ -1,56 +1,62 @@
 <template>
     <div class="secondary-header-container">
-    <header class="secondary-header big-search flex align-center justify-space-between">
-        <label class="flex column full">
-            <p>Where</p>
-            <input v-model="filterBy.name" type="text" placeholder="Search destinations">
-        </label>
-        <div class="break-line"></div>
-        <!-- <label class="flex column">
-            <p>Check in</p>
-        </label>
-        <div class="break-line"></div> -->
-        <label class="flex column">
-            <div class="check-in-out flex align-center justify-space-between">
-                <p>Check in</p>
-                <p>Check out</p>
-            </div>
-            <date-picker />
-        </label>
-        <div class="break-line"></div>
-        <div class="add-guests-container flex-container flex align-center justify-space-between full">
-            <label class="flex column">
-                <p>Who</p>
-                <span>Add guests</span>
+        <header 
+        :class="{selected:isElementSelected}"
+        class="secondary-header big-search flex align-center justify-space-between">
+            <label class="flex column full" ref="where" @click="selected('where')">
+                <p>Where</p>
+                <input v-model="filterBy.name" type="text" placeholder="Search destinations">
             </label>
-            <button @click="setFilterBy" class="search-container flex align-center justify-center">
-                <search-big />
-            </button>
-        </div>
-    </header>
-</div>
+            <div class="break-line"></div>
+            <label class="flex column date" ref="date" @click="selected('date')">
+                <div class="check-in-out flex align-center">
+                    <p class="full">Check in</p>
+                    <p class="full">Check out</p>
+                </div>
+                <date-picker />
+            </label>
+            <div class="break-line"></div>
+            <div ref="who" @click="selected('who')"
+                class="add-guests-container flex-container flex align-center justify-space-between full">
+                <label class="flex column">
+                    <p>Who</p>
+                    <span>Add guests</span>
+                </label>
+                <button @click="setFilterBy" class="search-container flex align-center justify-center">
+                    <search-big />
+                </button>
+            </div>
+        </header>
+    </div>
 
 </template>
 <script>
 import searchBig from '../assets/svg/search-big.vue';
-import { eventBus } from '../services/event-bus.service';
 import datePicker from './date-picker.vue';
 
 export default {
     name: 'secondary-header',
-    emits:['close-search'],
+    emits: ['close-search'],
     data() {
         return {
             filterBy: {
                 name: ''
-            }
+            },
+            isElementSelected:false
         }
     },
     methods: {
         setFilterBy() {
             const { name } = this.filterBy
-            this.$router.push({path:'/s', query:{name}})
-            // eventBus.emit('search',this.filterBy)
+            this.$router.push({ path: '/s', query: { name } })
+        },
+        selected(type) {
+            const els = this.$refs
+            for(const key in els){
+                els[key].classList.remove('selected')
+            }
+            this.$refs[type].classList.add('selected')
+            this.isElementSelected = true
         }
     },
     components: {
