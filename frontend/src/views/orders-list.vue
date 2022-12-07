bea46fe8ad965c061b05a714941af09145d79b13
Raz Israeli <razisraeli1@gmail.com>
2022-12-07 09:49:42 +0200

    commit before pull

--- frontend/src/views/orders-list.vue
+++ frontend/src/views/orders-list.vue
@@ -1,6 +1,6 @@
 <template>
-  <section v-if="orders" class='orders-list main-layout-list'>
-    <h1 class="fs22"> Reservation</h1>
+  <section v-if="orders" class="orders-list main-layout-list">
+    <h1 class="fs22">Reservation</h1>
     <div class="list">
       <table>
         <tr>
@@ -9,12 +9,18 @@
         <tr v-for="currOrder in orders">
           <td class="buyer-details-td flex align-center justify-center">
             <div class="buyer-img-container">
-              <img v-if="currOrder.buyer.imgUrl" :src="currOrder.buyer.imgUrl" alt="">
-              <user-avatar v-else/>
+              <img
+                v-if="currOrder.buyer.imgUrl"
+                :src="currOrder.buyer.imgUrl"
+                alt=""
+              />
+              <user-avatar v-else />
             </div>
-            <p>{{currOrder.buyer.fullname}}</p>
+            <p>{{ currOrder.buyer.fullname }}</p>
           </td>
-          <td :class="styleStatus(currOrder.status)" class="fs18 bold">{{ currOrder.status }}</td>
+          <td :class="styleStatus(currOrder.status)" class="fs18 bold">
+            {{ currOrder.status }}
+          </td>
           <td class="center-td">{{ formatGuests(currOrder.guests) }}</td>
           <td>{{ currOrder.startDate }}</td>
           <td>{{ currOrder.endDate }}</td>
@@ -22,7 +28,12 @@
           <td>{{ currOrder.stay.name }}</td>
           <td>{{ formatTotalPrice(currOrder.totalPrice) }}</td>
           <td>
-            <select v-model="currOrder.status" name="status" :value="currOrder.status" @change="updateOrder(currOrder)">
+            <select
+              v-model="currOrder.status"
+              name="status"
+              :value="currOrder.status"
+              @change="updateOrder(currOrder)"
+            >
               <option value="approved">Approved</option>
               <option value="pending">Pending</option>
               <option value="declined">Declined</option>
@@ -35,35 +46,44 @@
 
   <section v-if="orders" class="charts">
     <div class="chart-container">
-            <h3>Orders status average</h3>
-            <DoughnutChart :chartData="getStatusData" />
-        </div>
+      <h3>Orders status average</h3>
+      <DoughnutChart :chartData="getStatusData" />
+    </div>
   </section>
 </template>
 <script>
-import { DoughnutChart } from "vue-chart-3"
-import { Chart, registerables } from "chart.js"
+import { DoughnutChart } from 'vue-chart-3'
+import { Chart, registerables } from 'chart.js'
 
 import userAvatar from '../assets/svg/user-avatar.vue'
 
-Chart.register(...registerables);
+Chart.register(...registerables)
 
 export default {
-
   name: 'orders-list',
   props: {},
   data() {
     return {
-      tableHeadings: ['', 'Status', 'Guests', 'Check-in', 'Check-out', 'Booked', 'Listing', 'Total Payout', 'Actions'],
+      tableHeadings: [
+        '',
+        'Status',
+        'Guests',
+        'Check-in',
+        'Check-out',
+        'Booked',
+        'Listing',
+        'Total Payout',
+        'Actions',
+      ],
       orders: null,
       statusData: ['Approved', 'Pending', 'Declined'],
-            options: {
-                plugins: {
-                    legend: {
-                        display: false,
-                    }
-                }
-            }
+      options: {
+        plugins: {
+          legend: {
+            display: false,
+          },
+        },
+      },
     }
   },
   async created() {
@@ -84,7 +104,7 @@ export default {
       return new Date(createdAt).toLocaleDateString('en-US')
     },
     formatTotalPrice(totalPrice) {
-      return (totalPrice).toLocaleString('en-US', {
+      return totalPrice.toLocaleString('en-US', {
         style: 'currency',
         currency: 'USD',
       })
@@ -102,36 +122,32 @@ export default {
   },
   computed: {
     getStatusData() {
-            const data = this.statusData.map(status => {
-              const dataInNums = this.orders.reduce(
-                (acc, order) => {
-                  order.status === status
-                  ? acc + 1
-                  : acc,
-                  0
+      const data = this.statusData.map((status) => {
+        const dataInNums = this.orders.reduce((acc, order) => {
+          order.status === status ? acc + 1 : acc, 0
 
-                  // console.log(acc)
-                }
-                )
-                // console.log('dataInNums / this.statusData.length * 100', dataInNums / this.statusData.length * 100)
-                return dataInNums / this.statusData.length * 100
-            })
+          // console.log(acc)
+        })
+        // console.log('dataInNums / this.statusData.length * 100', dataInNums / this.statusData.length * 100)
+        return (dataInNums / this.statusData.length) * 100
+      })
 
-            return {
-              statusData: this.statusData,
-                datasets: [{
-                    status: 'Status Data',
-                    borderRadius: 6,
-                    data,
-                    backgroundColor: ["#77CEFF", "#0079AF", "#123E6B"]
-                }]
-            }
-        }
-
+      return {
+        statusData: this.statusData,
+        datasets: [
+          {
+            status: 'Status Data',
+            borderRadius: 6,
+            data,
+            backgroundColor: ['#77CEFF', '#0079AF', '#123E6B'],
+          },
+        ],
+      }
     },
+  },
   components: {
     userAvatar,
-    DoughnutChart
-  }
+    DoughnutChart,
+  },
 }
 </script>
