072cf4202f55b2089aea6ba02e1580a17d86f090
Bar Ben Shimol <barbenshimol2@gmail.com>
2022-12-11 14:18:15 +0200

    adding dynamin date to reviews

--- frontend/src/cmps/review.vue
+++ frontend/src/cmps/review.vue
@@ -4,7 +4,7 @@
       <img :src="review.by.imgUrl" alt="reviewer image" class="avatar" />
       <div class="details flex column">
         <h3 class="review-title">{{ review.by.fullname }}</h3>
-        <p class="review-subtitle">August 2022</p>
+        <p class="review-subtitle">{{formatDate}}</p>
       </div>
     </div>
     <div class="review-details">
@@ -26,7 +26,12 @@ export default {
   },
   created() {},
   methods: {},
-  computed: {},
+  computed: {
+    formatDate(){
+      const date = new Date(this.review.at)
+      return date.toLocaleString('en-us',{year:'numeric',month:'long'})
+    }
+  },
   components: {},
 }
 </script>
